3f0a03f2b44752aea56c970dbbd64f118ed1aea0
Jared Elliott <bootcamp@jaredclt.com>
2024-01-16 20:37:27 -0500

    Created CLI const and call the run function.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Where the other functions will be called
 // For initializing the project/application
+
+const CLI = require('./lib/cli.js');
+const cli = new CLI();
+
+// Run our interface for the user
+    cli.run();
