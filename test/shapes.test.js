b7c7805a1daf0f3ba4ab4beef865da7e68989326
Jared Elliott <bootcamp@jaredclt.com>
2024-01-17 22:27:13 -0500

    Cleaned up functions and test cases.

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -2,7 +2,7 @@ const shape = require('../lib/shapes.js');
 
 describe('Triangle', () => {
   test('should render a triangle with lime fill, black text, text as JAE.', () => {
-    const expectedShape = `<svg height="300" width="200"><polygon points="100,10 200,300 0,300" style="fill:lime;" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black" style="font-size: 2rem;">JAE</text></svg>`;
+    const expectedShape = `<svg version="1.1" height="300" width="200" xmlns="http://www.w3.org/2000/svg"><polygon points="100,10 200,300 0,300" style="fill:lime;" /><text x="50%" y="70%" dominant-baseline="middle" text-anchor="middle" fill="black" style="font-size: 2rem;">JAE</text></svg>`;
     const svgTriangle = new shape.Triangle('black','lime','JAE');
     expect(svgTriangle.render()).toEqual(expectedShape);
   });
@@ -10,18 +10,18 @@ describe('Triangle', () => {
 
 
 describe('Square', () => {
-  test('should render a square with lime fill, black text, text as JAE.', () => {
-    const expectedShape = `<svg height="300" width="200"><rect x="75" y="25" width="150" height="150" style="fill:lime" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black" style="font-size: 2rem;">JAE</text></svg>`;
-    const svgSquare = new shape.Square('black','lime','JAE');
+  test('should render a square with red fill, black text, text as JAE.', () => {
+    const expectedShape = `<svg version="1.1" height="300" width="200" xmlns="http://www.w3.org/2000/svg"><rect x="25" y="75" width="150" height="150" style="fill:red" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black" style="font-size: 2rem;">JAE</text></svg>`;
+    const svgSquare = new shape.Square('black','red','JAE');
     expect(svgSquare.render()).toEqual(expectedShape);
   });
 });
 
 
 describe('Circle', () => {
-  test('should render a circle with lime fill, black text, text as JAE.', () => {
-    const expectedShape = `<svg height="300" width="200"><circle cx="100" cy="150" r="100" fill="lime" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black" style="font-size: 2rem;">JAE</text></svg>`;
-    const svgCircle = new shape.Circle('black','lime','JAE');
+  test('should render a circle with blue fill, black text, text as JAE.', () => {
+    const expectedShape = `<svg version="1.1" height="300" width="200" xmlns="http://www.w3.org/2000/svg"><circle cx="100" cy="150" r="100" fill="blue" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black" style="font-size: 2rem;">JAE</text></svg>`;
+    const svgCircle = new shape.Circle('black','blue','JAE');
     expect(svgCircle.render()).toEqual(expectedShape);
   });
 });
