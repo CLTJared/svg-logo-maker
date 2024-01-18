1c5766d14229deb69c54ff7c623456b0c40039ac
Jared Elliott <bootcamp@jaredclt.com>
2024-01-17 21:27:20 -0500

    Updated functions & test cases with proper text.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Where the other functions will be called
 // For initializing the project/application
-
 const CLI = require('./lib/cli.js');
 const cli = new CLI();
-
 // Run our interface for the user
     cli.run();
--- test/shapes.test.js
+++ test/shapes.test.js
@@ -2,7 +2,7 @@ const shape = require('../lib/shapes.js');
 
 describe('Triangle', () => {
   test('should render a triangle with lime fill, black text, text as JAE.', () => {
-    const expectedShape = `<svg height="300" width="200"><polygon points="100,10 200,300 0,300" style="fill:lime;" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black" style="font-size: 2rem;">JAE</text>Sorry, your browser does not support inline SVG.</svg>`;
+    const expectedShape = `<svg height="300" width="200"><polygon points="100,10 200,300 0,300" style="fill:lime;" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black" style="font-size: 2rem;">JAE</text></svg>`;
     const svgTriangle = new shape.Triangle('black','lime','JAE');
     expect(svgTriangle.render()).toEqual(expectedShape);
   });
@@ -11,7 +11,7 @@ describe('Triangle', () => {
 
 describe('Square', () => {
   test('should render a square with lime fill, black text, text as JAE.', () => {
-    const expectedShape = `<svg height="300" width="200"><rect x="75" y="25" width="150" height="150" style="fill:lime" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black" style="font-size: 2rem;">JAE</text>Sorry, your browser does not support inline SVG.</svg>`;
+    const expectedShape = `<svg height="300" width="200"><rect x="75" y="25" width="150" height="150" style="fill:lime" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black" style="font-size: 2rem;">JAE</text></svg>`;
     const svgSquare = new shape.Square('black','lime','JAE');
     expect(svgSquare.render()).toEqual(expectedShape);
   });
@@ -20,7 +20,7 @@ describe('Square', () => {
 
 describe('Circle', () => {
   test('should render a circle with lime fill, black text, text as JAE.', () => {
-    const expectedShape = `<svg height="300" width="200"><circle cx="100" cy="150" r="100" fill="lime" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black" style="font-size: 2rem;">JAE</text>Sorry, your browser does not support inline SVG.</svg>`;
+    const expectedShape = `<svg height="300" width="200"><circle cx="100" cy="150" r="100" fill="lime" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black" style="font-size: 2rem;">JAE</text></svg>`;
     const svgCircle = new shape.Circle('black','lime','JAE');
     expect(svgCircle.render()).toEqual(expectedShape);
   });
